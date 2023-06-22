6da9c48312623ea95307ff991a4f9f2283ea3dfe
KarimAbdelnasser <kareemabdo38@yahoo.com>
2023-06-22 08:30:08 +0300

    Add auditing

--- src/middleware/audit.ts
+++ src/middleware/audit.ts
@@ -1,27 +1,11 @@
 import express, { Response } from "express";
 import AuditLog from "../models/audit.model";
-import { config } from "../config/config";
-import jwt, { Secret } from "jsonwebtoken";
 
 function auditMiddleware(
     req: express.Request & { user?: any },
     res: express.Response,
     next: express.NextFunction
 ) {
-    const token = req.header("x-auth-token");
-
-    if (!token) {
-        return res.status(401).send("Access denied, no token provided!");
-    }
-
-    const decoded = jwt.verify(token, config.jwt as Secret);
-
-    req.user = decoded;
-
-    if (!req.user) {
-        return res.status(401).json({ message: "Unauthorized" });
-    }
-
     const userId = req.user._id;
     const action = req.method + " " + req.path;
     const ipAddress = req.ip;
